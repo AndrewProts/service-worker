3a4888873a394d4632917c69f247302721973708
Andrew_Prots <Prozandrey@gmail.com>
2019-07-13 17:45:07 +0300

    asd

---
+++ precache-manifest.3ddba654a5981024f50c1231ceacf219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b294a50c7dd42e5cf454",
+    "url": "/js/chunk-vendors.79220dca.js"
+  },
+  {
+    "revision": "86d28d166269d703db72",
+    "url": "/js/app.98e55138.js"
+  },
+  {
+    "revision": "a5c83b678b863d95fb73788258255a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "86d28d166269d703db72",
+    "url": "/css/app.a2e8a372.css"
+  }
+];
